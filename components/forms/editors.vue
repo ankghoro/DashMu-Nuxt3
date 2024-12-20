99ad2ae434155fef83fe0713db903d8dc6e3683c
ankghoro <ankghoro@gmail.com>
2024-12-20 17:32:22 +0700

    add editor plugin tiptap

--- components/forms/editors.vue
+++ components/forms/editors.vue
@@ -1,26 +1,32 @@
 <template>
-	<div class="row">
-		<div class="col-lg-12">
-			<ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+	<div class="card card-secondary">
+		<div class="card-header">
+			<h3 class="card-title">Editor</h3>
+		</div>
+		<div class="card-body">
+			<div class="row">
+				<div class="col-lg-12">
+					<TipTapEditor v-model="editorData" />
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import TipTapEditor from '../TipTapEditor.vue'
 
 export default {
 	name: 'Editors',
 	components: {
-		//
+		TipTapEditor
 	},
 	methods: {
 		//
 	},
 	data() {
 		return {
-			editor: ClassicEditor,
-			editorData: '<p>Content of the editor.</p>',
+			editorData: '',
 			editorConfig: {
 				//
 			}
