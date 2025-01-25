6d815f8a8ada8d655702d69df7b0f472d795aa4f
ankghoro <ankghoro@gmail.com>
2025-01-25 17:59:31 +0700

    add database environment

--- server/db/index.ts
+++ server/db/index.ts
@@ -7,10 +7,13 @@ interface Options {
 }
 
 const connection = mysql.createPool({
-	host: 'localhost',
-	user: 'root',
-	password: 'root',
-	database: 'SPRIT_Portal',
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME,
+	waitForConnections: true,
+	connectionLimit: 10,
+	queueLimit: 0
 })
 
 export const sql = async ({query, values}: Options) => {
