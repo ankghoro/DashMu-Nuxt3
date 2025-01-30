c270eabe6767919cb60f713bc296135e63eeaa4f
ankghoro <ankghoro@gmail.com>
2025-01-30 18:10:02 +0700

    fixing chart js config

--- plugins/chart.ts
+++ plugins/chart.ts
@@ -1,27 +1,27 @@
-import { Chart } from 'chart.js'
-import { ArcElement } from 'chart.js'
-import { Tooltip } from 'chart.js'
-import { Legend } from 'chart.js'
-import { Bar, Pie, Line, Doughnut } from 'vue-chartjs'
+import { defineNuxtPlugin } from '#app';
+import {
+	Chart as ChartJS,
+	Title,
+	Tooltip,
+	Legend,
+	BarElement,
+	CategoryScale,
+	LinearScale,
+	LineElement,
+	PointElement,
+	ArcElement,
+} from 'chart.js';
 
-const dataDough = {
-  labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
-  datasets: [
-    {
-      backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
-      data: [40, 20, 80, 10]
-    }
-  ]
-}
-
-const options = {
-  responsive: true,
-  maintainAspectRatio: false
-}
-
-
-Chart.register(ArcElement, Tooltip, Legend)
-
-export default defineNuxtPlugin((nuxtApp) => {
-	nuxtApp.vueApp.component('pieChart', Pie)
-})
+export default defineNuxtPlugin(() => {
+	ChartJS.register(
+		Title,
+		Tooltip,
+		Legend,
+		BarElement,
+		CategoryScale,
+		LinearScale,
+		LineElement,
+		PointElement,
+		ArcElement
+	);
+});
